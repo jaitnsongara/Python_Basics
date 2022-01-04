0dd6f51929339347250e72c17b583168c64ba35d
jatin songara <jatin.songara@outlook.com>
2022-01-04 23:04:12 +0530

    # Basic Syntaxs in Python

---
+++ Learn_the_Basics/Basic_Syntax.py
@@ -0,0 +1,17 @@
+# Basic Syntaxs in Python 
+# Author: Jatin Songara
+# Comments are the guide for your program
+# ReadMe files have the Docs {Conceptes are here}
+#===================================================================================================================
+
+#-----------Taking the input into the vareable name here you can see------------------------------------------------
+name = input("enter your name\n")
+
+# here we are taking the name and 
+print(f"{name}, Hi ! this is a Pyhton program\n")
+
+
+
+#-----^ here you can see the ("f") stands for format, {name} is the variables other then this here is string 
+# What is string ?
+# String is set words numbers in the "string" 
