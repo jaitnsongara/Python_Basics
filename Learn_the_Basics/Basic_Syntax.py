1c41536fb05f97e21154ecbea3e1caf7527dbce3
jatin songara <jatin.songara@outlook.com>
2022-01-05 12:48:01 +0530

    Updated

--- Learn_the_Basics/Basic_Syntax.py
+++ Learn_the_Basics/Basic_Syntax.py
@@ -6,10 +6,9 @@
 
 #-----------Taking the input into the vareable name here you can see------------------------------------------------
 name = input("enter your name\n")
-
+#----^-- Assginement Opraters
 # here we are taking the name and 
 print(f"{name}, Hi ! this is a Pyhton program\n")
-
 
 
 #-----^ here you can see the ("f") stands for format, {name} is the variables other then this here is string 
